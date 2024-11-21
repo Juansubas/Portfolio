35a4ae6efe61215bbc321c2095bff60912ee1b68
juansubas <juansubas@gmail.com>
2024-11-21 01:20:43 -0500

    update

--- react-version/src/components/Layout.tsx
+++ react-version/src/components/Layout.tsx
@@ -3,7 +3,7 @@ import Footer from "./Footer";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="flex flex-col items-center justify-between w-full min-h-screen">
+    <main className="flex flex-col items-center justify-between w-11/12">
       <Header />
       {children}
       <Footer />
