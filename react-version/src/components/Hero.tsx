61d23b10b7cef973151f1cc722769842ec457532
juansubas <juansubas@gmail.com>
2024-11-21 00:01:05 -0500

    update

--- react-version/src/components/Hero.tsx
+++ react-version/src/components/Hero.tsx
@@ -1,7 +1,7 @@
 const Hero = () => {
   return (
     <section className="section mt-16">
-      <div className="grid md:grid-cols-[60%,40%] px-16 items-center truncate"> 
+      <div className="grid md:grid-cols-[60%,40%] px-16 items-center truncate">
         <div className="flex flex-col m-4 text-black truncate">
           <h1 className="text-7xl font-bold text-white">Hola Mundo</h1>
           <h2 className="text-7xl font-bold text-white truncate">
@@ -10,19 +10,23 @@ const Hero = () => {
               Juansubas
             </span>
           </h2>
-          {/* <h3 className="text-2xl font-bold text-white mt-2">
+          <h3 className="text-2xl font-bold text-white mt-2">
             YouTuber | Ingeniero Industrial | Desarrollador de Software
-          </h3> */}
-          {/* <p className="text-xl text-white pt-5 text-justify pr-3">
+          </h3>
+          <p className="text-xl text-white pt-5 text-justify pr-3">
             Soy un apasionado desarrollador con experiencia en React, Next.js,
             NestJS, Express y .NET. Me encanta crear soluciones innovadoras y
             compartir mi viaje como YouTuber, emprendedor y líder en el
             crecimiento personal. ¡Vamos a romperla juntos!
-          </p> */}
+          </p>
         </div>
 
         <div className=" text-black flex justify-center truncate">
-          <img src="./juansubas.png" alt="foto perfil" className="h-96 object-cover object-center" /> 
+          <img
+            src="./juansubas.png"
+            alt="foto perfil"
+            className="h-96 object-cover object-center"
+          />
         </div>
       </div>
     </section>
