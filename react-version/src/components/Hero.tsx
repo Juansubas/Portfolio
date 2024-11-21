c214caf9277c3367898d720c8e6b5de1ff5b2593
juansubas <juansubas@gmail.com>
2024-11-20 23:48:11 -0500

    Fix

--- react-version/src/components/Hero.tsx
+++ react-version/src/components/Hero.tsx
@@ -1,9 +1,8 @@
 const Hero = () => {
   return (
     <section className="section mt-16">
-      <div className="grid md:grid-cols-[60%,40%] px-16 items-center"> {/* Añadimos items-center para centrar el contenido */}
-        <h1>xd</h1>
-        {/* <div className="flex flex-col m-4 text-black">
+      <div className="grid md:grid-cols-[60%,40%] px-16 items-center"> 
+        <div className="flex flex-col m-4 text-black">
           <h1 className="text-7xl font-bold text-white">Hola Mundo</h1>
           <h2 className="text-7xl font-bold text-white">
             Soy{" "}
@@ -14,7 +13,7 @@ const Hero = () => {
           <h3 className="text-2xl font-bold text-white mt-2">
             YouTuber | Ingeniero Industrial | Desarrollador de Software
           </h3>
-          <p className="text-xl text-white pt-5 text-justify pr-3">
+          <p className="text-xl text-white pt-5 text-justify ">
             Soy un apasionado desarrollador con experiencia en React, Next.js,
             NestJS, Express y .NET. Me encanta crear soluciones innovadoras y
             compartir mi viaje como YouTuber, emprendedor y líder en el
@@ -24,7 +23,7 @@ const Hero = () => {
 
         <div className=" text-black flex justify-center">
           <img src="./juansubas.png" alt="foto perfil" className="h-96 object-cover object-center" /> 
-        </div> */}
+        </div>
       </div>
     </section>
   );
