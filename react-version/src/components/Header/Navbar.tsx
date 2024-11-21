59733a03cef7ccada99311ff5129b7c636cbe94d
juansubas <juansubas@gmail.com>
2024-11-20 22:08:04 -0500

    Update Repo

--- react-version/src/components/Header/Navbar.tsx
+++ react-version/src/components/Header/Navbar.tsx
@@ -1,7 +1,7 @@
 export const Navbar = () => {
   return (
     <nav>
-      <ul className="flex flex-col  md:flex-row  justify-center items-center gap-0 md:gap-2">
+      <ul className="flex flex-col  md:flex-row  justify-center items-center gap-0 md:gap-2 bg-[rgba(0,0,0,0.5)] backdrop-blur-md shadow-md">
         <li className="flex justify-center border md:border-none w-full md:w-auto">
           <a href="#projects" className="text-xl hover:text-slate-500">
             Projects
