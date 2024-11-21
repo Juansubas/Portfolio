013757e2dbd29e8e1f738d45c556462dd9493c6f
juansubas <juansubas@gmail.com>
2024-11-20 23:06:04 -0500

    Update Repo

--- react-version/src/components/Header/Brand.tsx
+++ react-version/src/components/Header/Brand.tsx
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 const Brand = () => {
   return (
     <div className="flex justify-center">
-      <Link to={'/'} className="text-xl hover:text-slate-500">
-        <h1>Juansubas</h1>
+      <Link to={"/"} className="text-xl hover:text-slate-500">
+        <h1 className="text-2xl font-bold bg-gradient-to-r from-gray-400 via-blue-900 to-blue-950 text-transparent bg-clip-text">
+          Juansubas
+        </h1>
       </Link>
     </div>
   );
--- react-version/src/components/Hero.tsx
+++ react-version/src/components/Hero.tsx
@@ -1,13 +1,26 @@
 const Hero = () => {
   return (
     <section className="section pt-5">
-      <div className="grid md:grid-cols-[60%,40%] ">
-        <div className="bg-gray-200 m-4 text-black">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est cum vel
-          et ratione accusamus, odit vero numquam, at officia ipsum quidem
-          dolorem! Accusantium necessitatibus consectetur praesentium, ratione
-          pariatur asperiores at.
+      <div className="grid md:grid-cols-[60%,40%] px-16">
+        <div className="flex flex-col  m-4 text-black">
+          <h1 className="text-7xl font-bold text-white">Hola Mundo</h1>
+          <h2 className="text-7xl font-bold text-white">
+            Soy{" "}
+            <span className="font-bold bg-gradient-to-r from-gray-400 via-blue-900 to-blue-950 text-transparent bg-clip-text">
+              Juansubas
+            </span>
+          </h2>
+          <h3 className="text-2xl font-bold text-white mt-2">
+            YouTuber | Ingeniero Industrial | Desarrollador de Software
+          </h3>
+          <p className="text-xl text-white pt-5 text-justify pr-3">
+            Soy un apasionado desarrollador con experiencia en React, Next.js,
+            NestJS, Express y .NET. Me encanta crear soluciones innovadoras y
+            compartir mi viaje como YouTuber, emprendedor y líder en el
+            crecimiento personal. ¡Vamos a romperla juntos!
+          </p>
         </div>
+
         <div className="bg-gray-300 m-4 text-black">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere
           vitae, voluptates aspernatur, est ex molestiae sed accusamus ipsum
