2853d2225b0f0570e1856bcb7b8e21d88413aad2
juansubas <juansubas@gmail.com>
2024-11-20 23:51:53 -0500

    Prueba foto

--- react-version/src/components/Hero.tsx
+++ react-version/src/components/Hero.tsx
@@ -3,8 +3,8 @@ const Hero = () => {
     <section className="section mt-16">
       <div className="grid md:grid-cols-[60%,40%] px-16 items-center"> 
         <div className="flex flex-col m-4 text-black">
-          {/* <h1 className="text-7xl font-bold text-white">Hola Mundo</h1>
-          <h2 className="text-7xl font-bold text-white">
+          <h1 className="text-7xl font-bold text-white">Hola Mundo</h1>
+          {/* <h2 className="text-7xl font-bold text-white">
             Soy{" "}
             <span className="font-bold bg-gradient-to-r from-gray-400 via-blue-900 to-blue-950 text-transparent bg-clip-text">
               Juansubas
