7904a9a5ff0757855443109c737aeb9f85ee6b02
juansubas <juansubas@gmail.com>
2024-11-20 23:50:55 -0500

    Prueba

--- react-version/src/components/Hero.tsx
+++ react-version/src/components/Hero.tsx
@@ -3,8 +3,8 @@ const Hero = () => {
     <section className="section mt-16">
       <div className="grid md:grid-cols-[60%,40%] px-16 items-center"> 
         <div className="flex flex-col m-4 text-black">
-          <h1 className="text-7xl font-bold text-white">Hola Mundo</h1>
-          {/* <h2 className="text-7xl font-bold text-white">
+          {/* <h1 className="text-7xl font-bold text-white">Hola Mundo</h1>
+          <h2 className="text-7xl font-bold text-white">
             Soy{" "}
             <span className="font-bold bg-gradient-to-r from-gray-400 via-blue-900 to-blue-950 text-transparent bg-clip-text">
               Juansubas
@@ -18,8 +18,8 @@ const Hero = () => {
             NestJS, Express y .NET. Me encanta crear soluciones innovadoras y
             compartir mi viaje como YouTuber, emprendedor y líder en el
             crecimiento personal. ¡Vamos a romperla juntos!
-          </p>
-        </div> */}
+          </p> */}
+        </div>
 
         <div className=" text-black flex justify-center">
           <img src="./juansubas.png" alt="foto perfil" className="h-96 object-cover object-center" /> 
