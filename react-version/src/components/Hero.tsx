1570290c39d5220cd76c95e2d6640aa60c818ca5
juansubas <juansubas@gmail.com>
2024-11-20 23:52:58 -0500

    prhase

--- react-version/src/components/Hero.tsx
+++ react-version/src/components/Hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
       <div className="grid md:grid-cols-[60%,40%] px-16 items-center"> 
         <div className="flex flex-col m-4 text-black">
           <h1 className="text-7xl font-bold text-white">Hola Mundo</h1>
-          {/* <h2 className="text-7xl font-bold text-white">
+          <h2 className="text-7xl font-bold text-white">
             Soy{" "}
             <span className="font-bold bg-gradient-to-r from-gray-400 via-blue-900 to-blue-950 text-transparent bg-clip-text">
               Juansubas
@@ -13,7 +13,7 @@ const Hero = () => {
           <h3 className="text-2xl font-bold text-white mt-2">
             YouTuber | Ingeniero Industrial | Desarrollador de Software
           </h3>
-          <p className="text-xl text-white pt-5 text-justify pr-3">
+          {/* <p className="text-xl text-white pt-5 text-justify pr-3">
             Soy un apasionado desarrollador con experiencia en React, Next.js,
             NestJS, Express y .NET. Me encanta crear soluciones innovadoras y
             compartir mi viaje como YouTuber, emprendedor y líder en el
