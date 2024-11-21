3c6365c1c9c5a856ae954388e52c87fdadc9ad24
juansubas <juansubas@gmail.com>
2024-11-21 01:54:57 -0500

    update project responsive

--- react-version/src/components/Hero.tsx
+++ react-version/src/components/Hero.tsx
@@ -1,6 +1,6 @@
 const Hero = () => {
   return (
-    <section id="hero" className="section mt-5 md:mt-16 mx-auto self-center">
+    <section id="hero" className="section mx-auto self-center">
       <div className="grid grid-cols-1 md:grid-cols-[60%,40%] px-5 md:px-16 items-start">
         <div className="flex flex-col m-4 text-black">
           <h1 className="text-4xl sm:text-5xl md:text-7xl font-bold text-white">Hola Mundo</h1>
