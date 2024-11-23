5e5c47cac6aca90314bb17ade40cdeae89fa80ad
juansubas <juansubas@gmail.com>
2024-11-23 07:43:13 -0500

    buttons transform and ScALE effect

--- react-version/src/components/Header/Brand.tsx
+++ react-version/src/components/Header/Brand.tsx
@@ -2,7 +2,7 @@ const Brand = () => {
   return (
     <div className="flex justify-center">
       <a href="#hero" className="text-xl hover:text-slate-500">
-        <h1 className="text-2xl font-bold bg-gradient-to-r from-gray-400 via-blue-900 to-blue-950 text-transparent bg-clip-text">
+        <h1 className="text-2xl font-bold bg-gradient-to-r from-gray-400 via-blue-900 to-blue-950 text-transparent bg-clip-text hover:scale-110 transition-all">
           Juansubas
         </h1>
       </a>
--- react-version/src/components/Header/Navbar.tsx
+++ react-version/src/components/Header/Navbar.tsx
@@ -3,17 +3,17 @@ export const Navbar = () => {
     <nav>
       <ul className="flex flex-col  md:flex-row  justify-center items-center gap-0 md:gap-2 bg-[rgba(0,0,0,0.5)] backdrop-blur-md shadow-md md:bg-transparent md:backdrop-blur-none md:shadow-none">
         <li className="flex justify-center border md:border-none w-full md:w-auto">
-          <a href="#projects" className="text-xl hover:text-slate-500">
+          <a href="#projects" className="text-xl hover:text-slate-500 hover:scale-110 transition-all">
             Projects
           </a>
         </li>
         <li className="flex justify-center border md:border-none w-full md:w-auto">
-          <a href="#contact" className="text-xl hover:text-slate-500">
+          <a href="#contact" className="text-xl hover:text-slate-500 hover:scale-110 transition-all">
             Contact
           </a>
         </li>
         <li className="flex justify-center border md:border-none w-full md:w-auto">
-          <a href="#about" className="text-xl hover:text-slate-500">
+          <a href="#about" className="text-xl hover:text-slate-500 hover:scale-110 transition-all">
             About
           </a>
         </li>
