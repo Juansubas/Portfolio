0a89806d3874ca123c40af059d82dfa8acb58938
juansubas <juansubas@gmail.com>
2024-11-23 08:26:48 -0500

    Update Data

--- react-version/src/components/Card.tsx
+++ react-version/src/components/Card.tsx
@@ -15,7 +15,7 @@ const Card = ({
     <section className="bg-gray-900 rounded-lg overflow-hidden shadow-lg transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl">
       <img src={img} alt="image" className="w-full h-56 object-cover" />
       <div className="p-5 flex flex-col justify-between h-56">
-        <div >
+        <div>
           <h1 className="text-2xl font-semibold text-white mb-3">{title}</h1>
           <p className="text-sm text-gray-300 line-clamp-3 mb-4">
             {description}
--- react-version/src/data/projectsData.ts
+++ react-version/src/data/projectsData.ts
@@ -23,31 +23,8 @@ export const projects = [
     img: Mint,
     title: "Todo",
     description: "Proyecto para gestionar las tareas del dia a dia.",
-    link: "https://www.otroproyecto3.com",
-    repo: "https://github.com/juansubas/otroproyecto3"
+    link: "https://www.todo.juansubas.com",
+    repo: "https://github.com/juansubas/todo"
   },
-  {
-    id: 4,
-    img: Mint,
-    title: "Mint",
-    description: "Mint fue una web realizada para un emprendimiento caleno en la cual se buscaba crear una agencia de marketing entre unos amigos",
-    link: "https://www.mint.juansubas.com",
-    repo: "https://github.com/juansubas/mint"
-  },
-  {
-    id: 5,
-    img: Mint,
-    title: "Ecommerce de Productos Masculinos",
-    description: "Un Ecommerce para adquirir productos de minoxidil y belleza masculina",
-    link: "https://www.otroproyecto.com",
-    repo: "https://github.com/juansubas/otroproyecto"
-  },
-  {
-    id: 6,
-    img: Mint,
-    title: "Todo",
-    description: "Proyecto para gestionar las tareas del dia a dia.",
-    link: "https://www.otroproyecto3.com",
-    repo: "https://github.com/juansubas/otroproyecto3"
-  }
+
 ];
